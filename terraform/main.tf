6b3d254890d875a21e487840b028391a0c28c2e7
Newton Sams0nov Scamander <egorrabota@ya.ru>
2023-10-11 17:10:58 +0300

    HW_terraform_01_commit (boyscout_fix)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,20 @@
-terraform {
-  required_version = ">=1.4.0"
+# terraform {
+#   required_version = ">=1.4.0"
 
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "0.90.0"
-    }
+#   required_providers {
+#     yandex = {
+#       source  = "yandex-cloud/yandex"
+#       version = "0.90.0"
+#     }
 
-    time = {
-      source  = "hashicorp/time"
-      version = "0.9.1"
-    }
+#     time = {
+#       source  = "hashicorp/time"
+#       version = "0.9.1"
+#     }
 
 
-  }
-}
+#   }
+# }
 
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
