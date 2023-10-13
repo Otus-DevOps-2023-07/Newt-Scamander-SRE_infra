e9becffaa60826f7b32550e1c634c4dfb5b97675
Newton Sams0nov Scamander <egorrabota@ya.ru>
2023-10-13 14:38:03 +0300

    terraform-2 fix and check

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,16 +3,14 @@ terraform {
 
   required_providers {
     yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "0.90.0"
+      source = "yandex-cloud/yandex"
+      version = "0.100.0"
     }
 
     time = {
       source  = "hashicorp/time"
       version = "0.9.1"
     }
-
-
   }
 }
 
@@ -22,4 +20,20 @@ provider "yandex" {
   folder_id                = var.folder_id
   zone                     = var.zone
 }
+module "app" {
+  source          = "./modules/app"
+  public_key_path = var.public_key_path
+  app_disk_image  = var.app_disk_image_id
+  subnet_id       = var.subnet_id
+  DB_connection   = module.db.external_ip_address_db
+}
+
+module "db" {
+  source          = "./modules/db"
+  public_key_path = var.public_key_path
+  db_disk_image   = var.db_disk_image_id
+  subnet_id       = var.subnet_id
+}
+
+
 resource "time_static" "example" {}
