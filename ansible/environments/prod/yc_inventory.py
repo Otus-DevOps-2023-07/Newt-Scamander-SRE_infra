de94c1a56bb4624fd8ec7c8b51efc877112497b1
Newton Sams0nov Scamander <egorrabota@ya.ru>
2023-10-22 00:31:57 +0300

    Ansible-3. First commit

--- ansible/environments/prod/yc_inventory.py
+++ ansible/environments/prod/yc_inventory.py
@@ -15,8 +15,15 @@ dynamic_inventory = {
         "hosts": [],
         "vars": {
             "ansible_connection": "ssh",
-            "ansible_user": "ubuntu"
+            "ansible_user": "ubuntu",
+            "env": "prod"
         }
+    },
+    "app": {
+        "hosts": [],
+    },
+    "db": {
+        "hosts": [],
     }
 }
 
@@ -40,10 +47,16 @@ for instance in instances:
     internal_ip = instance["network_interfaces"][0]["primary_v4_address"]["address"]
 
     dynamic_inventory["_meta"]["hostvars"][instance_name] = {
-        "ansible_host": external_ip
+        "ansible_host": external_ip,
+        "internal_ip": internal_ip
     }
 
     dynamic_inventory["all"]["hosts"].append(instance_name)
+
+    if "app" in instance_name:
+        dynamic_inventory["app"]["hosts"].append(instance_name)
+    elif "db" in instance_name:
+        dynamic_inventory["db"]["hosts"].append(instance_name)
 
     # Add host to the static inventory
     static_inventory["all"]["hosts"][instance_name] = {
@@ -69,10 +82,11 @@ for instance in instances:
 # Print the dynamic inventory for Ansible to use
 print(json.dumps(dynamic_inventory, indent=4))
 
+
 # Save the dynamic inventory to a JSON file
-with open("dynamic_inv_output.json", "w") as dynamic_json_file:
+with open("./dynamic_inv_output.json", "w") as dynamic_json_file:
     json.dump(dynamic_inventory, dynamic_json_file, indent=4)
 
 # Save the static inventory to a JSON file
-with open("static_inventory.json", "w") as static_json_file:
+with open("./static_inventory.json", "w") as static_json_file:
     json.dump(static_inventory, static_json_file, indent=4)
